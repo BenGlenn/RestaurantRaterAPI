28cd54673276cc6171d69036718f8aac69a9f9cf
BenGlenn <68648238+BenGlenn@users.noreply.github.com>
2020-08-25 10:43:41 -0400

    second Commit

--- RestaurantRaterAPI/Models/Restaurant.cs
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -1,11 +1,51 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace RestaurantRaterAPI.Models
 {
+    // This is the Restaurant ENTITY ( the class that gets stored in the database)
+
     public class Restaurant
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+
+        public bool IsRecommended 
+        {
+            get
+            {
+                // public bool IsRecommended => Rating > 3.5;
+
+                return Rating > 3.5;
+
+                // SAME THING for all 4 iterations 
+
+                //return (Rating > 3.5) ? true : false;
+
+                ////SAME THING
+
+                //if(Rating > 3.5)
+                //{
+                //    return true;
+                //}
+                //else
+                //{
+                //    return false;
+                //}
+            }
+        
+        }
+
     }
 }
