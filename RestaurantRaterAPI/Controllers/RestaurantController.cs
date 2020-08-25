a9062f112c088aa8b6cbbc794a8371a7021da28e
BenGlenn <68648238+BenGlenn@users.noreply.github.com>
2020-08-25 15:48:01 -0400

    Adding Update

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -38,6 +38,18 @@ namespace RestaurantRaterAPI.Controllers
         //-- Read (GET)
         // Get by ID
 
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+            return NotFound();
+        }
+
         // Get All
         [HttpGet]
         public async Task<IHttpActionResult> GetAll()
@@ -47,6 +59,33 @@ namespace RestaurantRaterAPI.Controllers
         }
 
         //-- Update (PUT)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri]int id, [FromBody]Restaurant updatedRestaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                // Find and update the appropriate restaurant
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+                
+                if (restaurant != null)
+                {
+                    // Update the restaurant 
+                    restaurant.Name = updatedRestaurant.Name;
+                    restaurant.Rating = updatedRestaurant.Rating;
+
+                    await _context.SaveChangesAsync();
+
+                    return Ok("Restaurant has been updated");
+                }
+
+                // Didn't find the restaurant
+
+                return NotFound();
+            }
+
+            // Return a bad request
+            return BadRequest(ModelState);
+        }
 
         //-- Delete (DELETE)
     }
